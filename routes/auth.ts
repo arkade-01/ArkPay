4711602f89dfe95b7393a7b219e2364b8a350357
arkade-01 <noabiliaminfirst@gmail.com>
2025-04-24 23:42:00 +0100

    protected all Auth routes

--- routes/auth.ts
+++ routes/auth.ts
@@ -5,19 +5,19 @@ import { protectRoute } from "../middlewares/auth"
 const authRouter = express()
 
 //Signup Route
-authRouter.post('/signup',signup )
+authRouter.post('/signup', protectRoute,signup )
 
 // Signin Route
-authRouter.post('/signin', signin)
+authRouter.post('/signin', protectRoute, signin)
 
 // Signout Route
-authRouter.post('/signout', signout)
+authRouter.post('/signout', protectRoute, signout)
 
 // Forgot Password
-authRouter.post('/forgot-password', forgotPassword)
+authRouter.post('/forgot-password', protectRoute, forgotPassword)
 
 //Reset Password 
-authRouter.post('/reset-password', resetPassword)
+authRouter.post('/reset-password', protectRoute, resetPassword)
 
 //Generate new API key
 authRouter.post('/generate-api-key', protectRoute, resetAPIKey)
